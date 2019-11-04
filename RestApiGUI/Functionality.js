ba2a0fa8a3535bac15add9a4d29462b7fc02f0b6
alexelm <elmgren94@live.se>
2019-11-04 14:09:54 +0100

    RestAPI
    First rest api attempt

---
+++ RestApiGUI/Functionality.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var AddButton = document.getElementById("AddButton");
+var DeleteButton = document.getElementById("DeleteButton");
+
+AddButton.addEventListener("click", function(){AddButton.colspan="6");
+
+
+
